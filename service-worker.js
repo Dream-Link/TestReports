ec16e040fa9fb3ab669b5a055a5161c4f9f6962f
Dream-Link <kavitacloud7@gmail.com>
2025-08-28 10:15:30 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,63 @@
+// v1 of the service worker
+const CACHE_NAME = 'test-tracker-cache-v1';
+// List of files that make up the app shell
+const urlsToCache = [
+  '.', // This caches the root URL
+  'tracker.html',
+  'manifest.json',
+  'icon-192x192.png',
+  'icon-512x512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css'
+];
+
+/**
+ * Install the service worker and cache the app shell.
+ */
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache and caching app shell');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+/**
+ * Clean up old caches on activation.
+ */
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          // If the cache name is not in our whitelist, delete it
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+/**
+ * Serve cached content when offline, and fall back to the network.
+ * This is a "cache-first" strategy.
+ */
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return the response from the cache
+        if (response) {
+          return response;
+        }
+        // Not in cache - fetch from the network
+        return fetch(event.request);
+      }
+    )
+  );
+});
